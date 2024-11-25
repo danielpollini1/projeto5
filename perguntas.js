c9ca6a80089ffd7fa86d82d1f5af2d2a07daee5a
danielpollini1 <danieleduardopoll07@gmail.com>
2024-11-25 14:22:00 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'História',
+    'Quais foram os dois maiores partidos da Revolução Russa?',
+    'Os Bolcheviques e os Mencheviques'
+)
+
+criaCartao(
+    'Literatura',
+    'Qual o maior nome da literatura Russa?',
+    'O maior nome da literatura Russa é Fiodor Dostoievski'
+)
+
+criaCartao(
+    'Geografia ',
+    'Qual a Capital da Rússia?',
+    'A Capital da Rússia é Moscou'
+)
+
+criaCartao(
+    'Lingua francesa',
+    'Como se diz oi, como vai? Em Francês',
+    'Oi, como vai em francês é "Salut, ça va?'
+)
